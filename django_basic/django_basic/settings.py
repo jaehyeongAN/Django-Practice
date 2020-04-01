f4ef580d3803cbaae674323d2d338f17b75166ab
jaehyeongAN <nonamed000000@gmail.com>
2020-04-02 00:41:06 +0900

    pythonanywhere

--- django_basic/django_basic/settings.py
+++ django_basic/django_basic/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'm96e$^jm(*@gxcpokjcs!oe1)+&zm6z@tiv@eryk9^pdl%bp+b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['54.180.150.154','127.0.0.1','0.0.0.0']
+ALLOWED_HOSTS = [
+    'jaehyeong.pythonanywhere.com'
+]
 
 
 # Application definition
@@ -121,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static') # static 파일의 경로를 알려줌 
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static') # static 파일의 경로를 알려줌 
+# ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
