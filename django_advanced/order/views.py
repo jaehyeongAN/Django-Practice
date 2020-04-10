4810a95e93abbb40c2c6f082e714541ad4723afe
jaehyeongAN <nonamed000000@gmail.com>
2020-04-11 00:03:03 +0900

    order create

--- django_advanced/order/views.py
+++ django_advanced/order/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic.edit import FormView
+from .forms import RegisterForm
 
 # Create your views here.
+
+class OrderCreate(FormView):
+	# ProductDetail에서 form을 받기 때문에 template_name이 필요하지 않음
+	form_class = RegisterForm
+	success_url = '/product/'
--- django_advanced/product/views.py
+++ django_advanced/product/views.py
@@ -3,6 +3,7 @@ from django.views.generic import ListView
 from django.views.generic.edit import FormView, DeleteView
 from .models import Product
 from .forms import RegisterForm
+from order.forms import RegisterForm as OrderForm
 
 # Create your views here.
 
@@ -21,3 +22,10 @@ class ProductDetail(DeleteView):
 	template_name = 'product_detail.html'
 	queryset = Product.objects.all()
 	context_object_name = 'product'
+
+	# 위의 기존 form(ProductForm)을 먼저 수행 후 새로운 form(OrderForm) 가져옴 
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+
+		context['form'] = OrderForm()
+		return context
